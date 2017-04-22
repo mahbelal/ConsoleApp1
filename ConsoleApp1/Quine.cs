5e79e23f496b9ba47351f350a43978b5c5b2418c
Mahajna Belal <Mahajna Belal>
2017-04-22 10:57:16 +0300

    Add project files.

---
+++ ConsoleApp1/Quine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Quine
+    {
+        public static string Esc(string code)
+        {
+            return code.Replace("\n","\\n\" + \"");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(code + Esc(code) + ";\n   }\n}");
+        }
+
+        public static string code = "using System;\n" + "\n" + "namespace ConsoleApp1\n" + "{\n" + "   public class Quine\n" +
+                                    "   {\n" + "      public static string Esc(string code)\n" + "      {\n" +
+                                    "         return code.Replace(\"\\n\",\"\\\\n\\\" + \\\"\");\n" + "      }\n" + "\n" +
+                                    "      public static void Main(string[] args)\n" + "      {\n" +
+                                    "         Console.WriteLine(code + Esc(code) + \";\\n   }\\n}\");\n" + "      }\n" +
+                                    "\n" + "      public static string code = \\\"";
+    }
+}
